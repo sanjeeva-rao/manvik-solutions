d422b856b9b66397289a5dffdbcee2bc4a956401
sanjeeva-rao <sanjeevarao436@gmail.com>
2024-11-15 19:14:53 +0530

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return <div className="App-header">
+        <p className="header-items">Manvik Solutions</p>
+        <p className="header-items">Services</p>
+        <p className="header-items">Prices</p>
+        <p className="header-items">Samples</p>
+        <p className="header-items">Our writers</p>
+        <p className="header-items">Reviews</p>
+        <p className="header-items">More</p>
+        <button className="order-button">Order Now</button>
+    </div>
+}
+
+export default Header;
